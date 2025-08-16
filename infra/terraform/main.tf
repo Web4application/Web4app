0e2b46cdffd775515c515401e208f1f287c1a9a4
Web4 <167559384+Web4application@users.noreply.github.com>
2025-08-16 02:23:32 +0100

    Update main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,3 +1,14 @@
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
+}
+
+resource "aws_db_instance" "web4_db" {
+  allocated_storage    = 20
+  engine               = "postgres"
+  engine_version       = "15"
+  instance_class       = "db.t3.micro"
+  name                 = "web4"
+  username             = var.db_user
+  password             = var.db_password
+  skip_final_snapshot  = true
 }
