7026d28c71b374ded4e239e41a5235213b530074
Web4 <167559384+Web4application@users.noreply.github.com>
2025-08-16 02:38:42 +0100

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -1,3 +1,4 @@
 xcode-select -s /Applications/Xcode-beta.app
 xcodebuild -runFirstLaunch
 xcodebuild -importPlatform "~/Downloads/watchOS 9 beta Simulator Runtime.dmg"
+uvicorn services.assistant.app.main:app --reload --host 0.0.0.0 --port 8000
