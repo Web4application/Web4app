bfabc3348ead922fd22a6eecb541ec2f3de56dc9
Web4 <167559384+Web4application@users.noreply.github.com>
2025-09-27 07:49:14 +0100

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -2,3 +2,6 @@ xcode-select -s /Applications/Xcode-beta.app
 xcodebuild -runFirstLaunch
 xcodebuild -importPlatform "~/Downloads/watchOS 9 beta Simulator Runtime.dmg"
 uvicorn services.assistant.app.main:app --reload --host 0.0.0.0 --port 8000
+
+$ echo $CR_PAT | docker login ghcr.io -u web4application --password-mMankind1$&@"
+> Login Succeeded
